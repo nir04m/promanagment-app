49b11d5c0a1b94661d6ac779bcab83267e79ffa5
Miro <mairoenwa@gmail.com>
2026-03-14 09:55:33 -0600

    Update prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,16 +1,15 @@
-import path from 'path';
-import { defineConfig } from 'prisma/config';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in environment variables');
-}
+import path from "path";
+import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
+  schema: path.join("prisma", "schema.prisma"),
+
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: env("DATABASE_URL"),
+  },
+
+  migrations: {
+    path: path.join("prisma", "migrations"),
   },
 });
