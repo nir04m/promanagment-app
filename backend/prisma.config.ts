13d92ec1788d091e676d1db015824f2e903318d5
Miro <mairoenwa@gmail.com>
2026-03-14 10:18:49 -0600

    Update prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,6 +1,6 @@
+import "dotenv/config";
 import path from "path";
 import { defineConfig } from "prisma/config";
-import "dotenv/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
